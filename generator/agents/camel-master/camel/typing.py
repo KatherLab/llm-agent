1c0cd98446793fab579c26f056d1977b4054083e
Jeff <jiefu.zhu@tu-dresden.de>
2023-07-25 14:44:43 +0200

    add camel

---
+++ generator/agents/camel-master/camel/typing.py
@@ -0,0 +1,66 @@
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+from enum import Enum
+
+
+class RoleType(Enum):
+    ASSISTANT = "assistant"
+    USER = "user"
+    CRITIC = "critic"
+    EMBODIMENT = "embodiment"
+    DEFAULT = "default"
+
+
+class ModelType(Enum):
+    GPT_3_5_TURBO = "gpt-3.5-turbo"
+    GPT_3_5_TURBO_16K = "gpt-3.5-turbo-16k"
+    GPT_4 = "gpt-4"
+    GPT_4_32k = "gpt-4-32k"
+    STUB = "stub"
+
+    @property
+    def value_for_tiktoken(self) -> str:
+        return self.value if self.name != "STUB" else "gpt-3.5-turbo"
+
+    @property
+    def token_limit(self) -> int:
+        r"""Returns the maximum token limit for a given model.
+        Returns:
+            int: The maximum token limit for the given model.
+        """
+        if self is ModelType.GPT_3_5_TURBO:
+            return 4096
+        elif self is ModelType.GPT_3_5_TURBO_16K:
+            return 16384
+        elif self is ModelType.GPT_4:
+            return 8192
+        elif self is ModelType.GPT_4_32k:
+            return 32768
+        elif self is ModelType.STUB:
+            return 4096
+        else:
+            raise ValueError("Unknown model type")
+
+
+class TaskType(Enum):
+    AI_SOCIETY = "ai_society"
+    CODE = "code"
+    MISALIGNMENT = "misalignment"
+    TRANSLATION = "translation"
+    EVALUATION = "evaluation"
+    SOLUTION_EXTRACTION = "solution_extraction"
+    DEFAULT = "default"
+
+
+__all__ = ['RoleType', 'ModelType', 'TaskType']
